15d74fff1794d78f9173b258e5104d11a2e23b60
Kevin Rohling <kevin@kevinrohling.com>
2025-04-01 11:51:21 -0700

    skeleton, nothing functional yet

---
+++ setup.sh
@@ -0,0 +1,23 @@
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+source ~/miniconda3/bin/activate
+
+conda create -n rpd python=3.10 -y
+conda rpd openvla
+
+git clone https://github.com/Lifelong-Robot-Learning/LIBERO.git
+git clone https://github.com/openvla/openvla.git
+
+cd openvla
+pip install -e .
+
+cd ../LIBERO
+pip install -e .
+
+cd ../openvla
+pip install -r experiments/robot/libero/libero_requirements.txt
+
+cd ../refined-policy-distillation
+pip install -r requirements-ppo.txt
